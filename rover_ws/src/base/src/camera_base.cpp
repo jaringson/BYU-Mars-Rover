5a161a0282f8f6370226fbb437cf092fc8cc1c14
dwes7 <dwestley.1992@gmail.com>
2016-10-14 10:51:41 -0600

    camera_base node can listen to usb_cam/image_raw topic

--- rover_ws/src/base/src/camera_base.cpp
+++ rover_ws/src/base/src/camera_base.cpp
@@ -3,18 +3,19 @@
 #include <image_transport/image_transport.h>
 #include <sstream>
 
-int[5] cam_array = [0,0,0,0,0];
+// int cam_array = [0,0,0,0,0];
 
 /*
 callback to set values of cam_array. 1 indicates that camera (n) is being sampled by rover camera
 controller and its images are being published as compressed images over the rocket
 */
-void cmd_callback(const std::String::ConstPtr& msg){
-	cam_array = msg;		//must change b/c rover_command will have lots of fields
-}
+// void cmd_callback(const std_msgs::String::ConstPtr& msg){
+// 	cam_array = msg;		//must change b/c rover_command will have lots of fields
+// }
 
 // decompress image and republish
 void image_callback(const sensor_msgs::ImageConstPtr& msg){
+	ROS_INFO_STREAM("message recieved");
 	
 }
 
@@ -24,20 +25,22 @@ int main(int argc, char **argv){
 
 	//node handle
 	ros::NodeHandle nh;
+	//nh.param<image_transport::compressed>("compress_image", )
 	image_transport::ImageTransport it(nh);
 	
 	//subscriber for camera commands
-	ros::Subscriber cmd_sub = nh.subscribe("rover_command", 1000, cmd_callback);
+	//ros::Subscriber cmd_sub = nh.subscribe("rover_command", 1000, cmd_callback);
 	
-	for (int i=0;i<5,i++){
-		if cam_array[i] == 1{
-			stringstream ss;
-			ss << "cam_" << i << "/compressed";
-			node_name = ss.str();
+	// for (int i=0;i<5,i++){
+	// 	if cam_array[i] == 1{
+	// 		stringstream ss;
+	// 		ss << "cam_" << i << "/compressed";
+	// 		node_name = ss.str();
 			
-			image_transport::Subscriber img_sub = it.subscribe(node_name,1,image_callback);
-		}
-	}
-	
+	// 		image_transport::Subscriber img_sub = it.subscribe(node_name,1,image_callback);
+	// 	}
+	// }
+	image_transport::Subscriber img_sub = it.subscribe("usb_cam/image_raw",1000,image_callback);
+
 	ros::spin();
 }
