9a852075c85f75d4fd0b68e0e4d8a8e90122ac8c
dwes7 <dwestley.1992@gmail.com>
2016-10-18 13:57:17 -0600

    node now subscribes to joy

--- rover_ws/src/base/scripts/camera_base.py
+++ rover_ws/src/base/scripts/camera_base.py
@@ -2,7 +2,7 @@
 import rospy
 from std_msgs.msg import String
 from sensor_msgs.msg import CompressedImage
-from sensor_msgs.msg import joy
+from sensor_msgs.msg import Joy
 
 
 """
@@ -10,6 +10,10 @@ This node is meant to receive compressed image data from all 5 cameras on the ro
 decide which compressed images to send to the control station. 
 
 """
+def joy_callback(data):
+	button_pushed = data
+	# rospy.loginfo(rospy.get_caller_id() + " button pushed? ", data)
+	print("data = ", data.buttons[0])
 
 def image_callback(data):
 	rospy.loginfo(rospy.get_caller_id() + "hello world")
@@ -18,11 +22,12 @@ def image_callback(data):
 def cam_hub():
 
 	rospy.init_node('cam_hub', anonymous=True)
-
 	
-	rospy.Subscriber("usb_cam0/image_raw/compressed",CompressedImage, image_callback)
-	rospy.Subscriber("usb_cam1/image_raw/compressed",CompressedImage, image_callback)
-
+	
+	rospy.Subscriber("joy", Joy, joy_callback)
+	# rospy.Subscriber("usb_cam0/image_raw/compressed",CompressedImage, image_callback)
+	# rospy.Subscriber("usb_cam1/image_raw/compressed",CompressedImage, image_callback)
+	
 	rospy.spin()
 	
 if __name__ == '__main__':
