a0d339217612dfe5c98586c08ad05d1ea5228d8b
dwes7 <dwestley.1992@gmail.com>
2016-11-02 09:54:58 -0600

    met with Parker from Magicc lab. Helped us with syntax.

---
+++ rover_ws/src/base/src/rover_hub_node.cpp
@@ -0,0 +1,26 @@
+#include "ros/ros.h"
+#include "rover_hub.h"
+
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "rover_hub_node");
+
+    ros::NodeHandle nh;
+    ros::NodeHandle nh_private("~");
+    int rate;
+    nh_private.param<int>("rate", rate, 10);
+
+    rover_hub::Rover_hub rh;
+    ros::Rate loop_rate(rate);
+    while(ros::ok())
+    {
+        ros::spinOnce();
+//        ro.publish_image();
+
+        loop_rate.sleep();
+        //ROS_WARN_STREAM("Spinning");
+    }
+
+//    ros::spin();
+    return 0;
+}
