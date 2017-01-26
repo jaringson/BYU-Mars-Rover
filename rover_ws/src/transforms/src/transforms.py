b4f5804eef9e32acc421ba49fd761921af39913a
Brian Jackson <bjack205@gmail.com>
2017-01-26 08:25:55 -0700

    Add Transforms node

---
+++ rover_ws/src/transforms/src/transforms.py
@@ -0,0 +1,15 @@
+import roslib
+import rospy
+import tf
+
+if __name__ == '__main__':
+    rospy.init_node('rover_transforms')
+    br = tf.TransformBroadcaster()
+    rate = rospy.Rate(10.0)
+    while not ros.is_shutdown():
+        # LiDar to Base Frame
+        br.sendTransform((0.0, 0.0, 0.0),
+                         (0.0, 0.0, 0.0, 1.0),
+                         "LiDar",
+                         "rover_base")
+        rate.sleep()
