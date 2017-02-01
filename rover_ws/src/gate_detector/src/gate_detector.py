bcd354b158d859da4624f68a96e7a8b377a58185
dwes7 <dwestley.1992@gmail.com>
2017-02-01 08:33:55 -0700

    got the gate detector working in ROS. Still needs some work

--- rover_ws/src/gate_detector/src/gate_detector.py
+++ rover_ws/src/gate_detector/src/gate_detector.py
@@ -4,37 +4,57 @@ import roslib
 import cv2
 import numpy as np
 import rospy, sys, time
+from std_msgs.msg import Bool
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
 
 
-class image_converter:
+class gate_detector:
     def __init__(self):
 
-        self.image_pub = rospy.Publisher("gate_detector/image_raw", Image, queue_size=10)
+        self.detector_pub = rospy.Publisher("gate_detector/image_detector", Image, queue_size=10)
+        self.hsv_pub = rospy.Publisher("gate_detector/hsv", Image, queue_size=10)
+        self.isDetected_pub = rospy.Publisher("gate_detector/isDetected", Bool, queue_size = 1)
+
         self.bridge = CvBridge()
         self.image_sub = rospy.Subscriber("usb_cam/image_raw", Image, self.callback)
-        self.init_trackbars
+        self._hl = rospy.get_param('_hue_lower', 28)
+        self._hu = rospy.get_param('_hue_upper', 54)
+        self._sl = rospy.get_param('_sat_lower', 136)
+        self._su = rospy.get_param('_sat_upper', 255)
+        self._vl = rospy.get_param('_val_lower', 111)
+        self._vu = rospy.get_param('_val_upper', 255)
+        self.isDetected = False
+
 
     def callback(self, data):
+        
         print('getting here')
         img_msg = data
         print("got data")
         try:
-            cv_image = self.bridge.imgmsg_to_cv2(img_msg, desired_encoding="bgr8")
+            print("got the cv_image")
+            cv_image = self.bridge.imgmsg_to_cv2(img_msg, desired_encoding="rgb8")
         except CvBridgeError as e:
+            print("goterror")
             print(e)
 
+        # cv_image = cv2.cvtColor(cv_image, cv2.)    
         # this command flips the frame around the y axis
-        frame = cv2.flip(cv_image, 1)
 
+        frame = cv_image
+        lower =  np.array([self._hl, self._sl, self._vl])
+        upper =  np.array([self._hu, self._su, self._vu])
         blur = cv2.medianBlur(cv_image, 3)
-        hsv = cv2.cvtColor(cv_image, cv2.COLOR_BGR2HSV)
-        thrImg = self.getthresholdedimg(hsv)
+        hsv = cv2.cvtColor(cv_image, cv2.COLOR_RGB2HSV)
+        thrImg = cv2.inRange(hsv,lower,upper)
+
         erode = cv2.erode(thrImg, None, iterations=2)
         dilate = cv2.dilate(erode, None, iterations=2)
 
         contours, hierarchy = cv2.findContours(dilate, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+
+        self.isDetected = False
         for cnt in contours:
             x, y, w, h = cv2.boundingRect(cnt)
             cx, cy = x + w / 2, y + h / 2
@@ -42,7 +62,7 @@ class image_converter:
             if (abs(h * w)**2) > cv2.getTrackbarPos('Box filter', 'Trackbars'):
 
                 cv2.rectangle(frame, (x, y), (x + w, y + h), [0, 0, 255], 2)
-                print "******tennis ball detected!!***********"
+                self.isDetected = True
 
         if cv2.getTrackbarPos('Caliberate', 'Trackbars') == 1:
             cv2.imshow('Output', thrImg)
@@ -50,90 +70,25 @@ class image_converter:
             cv2.imshow('Output', frame)
 
         try:
-            self.image_pub.publish(self.bridge.cv2_to_imgmsg(thrImg, "bgr8"))
+            self.isDetected_pub.publish(self.isDetected)
+            self.detector_pub.publish(self.bridge.cv2_to_imgmsg(frame, "rgb8"))
+            self.hsv_pub.publish(self.bridge.cv2_to_imgmsg(thrImg, "mono8"))
+
         except CvBridgeError as e:
             print(e)
 
 
-    def init_trackbars(self):
-        # cv2.namedWindow('Output')
-        cv2.namedWindow('Trackbars', cv2.WINDOW_NORMAL)
-        cv2.createTrackbar('Hue_Low', 'Trackbars', 0, 255, getTrackValue)
-        cv2.createTrackbar('Hue_High', 'Trackbars', 0, 255, getTrackValue)
-        cv2.createTrackbar('Value_Low', 'Trackbars', 0, 255, getTrackValue)
-        cv2.createTrackbar('Value_High', 'Trackbars', 0, 255, getTrackValue)
-        cv2.createTrackbar('Saturation_Low', 'Trackbars', 0, 255, getTrackValue)
-        cv2.createTrackbar('Saturation_High', 'Trackbars', 0, 255, getTrackValue)
-        cv2.createTrackbar('Caliberate', 'Trackbars', 0, 1, getTrackValue)
-        cv2.createTrackbar('Box filter', 'Trackbars', 0, 10000, getTrackValue)
-
-    def getthresholdedimg(self, hsv):
-        # type: (object) -> object
-        threshImg = cv2.inRange(hsv, np.array((cv2.getTrackbarPos('Hue_Low', 'Trackbars'),
-                                               cv2.getTrackbarPos('Saturation_Low', 'Trackbars'),
-                                               cv2.getTrackbarPos('Value_Low', 'Trackbars'))), np.array((cv2.getTrackbarPos(
-            'Hue_High', 'Trackbars'), cv2.getTrackbarPos('Saturation_High', 'Trackbars'), cv2.getTrackbarPos('Value_High',
-                                                                                                             'Trackbars'))))
-
-        return threshImg
-
-
-    def getTrackValue(value):
-        return value
-
-
-# while (1):
-#     grabbed, frame = c.read()
-#     # this command flips the frame around the y axis
-#     frame = cv2.flip(frame, 1)
-
-#     blur = cv2.medianBlur(frame, 3)
-#     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-#     thrImg = getthresholdedimg(hsv)
-#     erode = cv2.erode(thrImg, None, iterations=2)
-#     dilate = cv2.dilate(erode, None, iterations=2)
-
-#     contours, hierarchy = cv2.findContours(dilate, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
-
-#     for cnt in contours:
-#         x, y, w, h = cv2.boundingRect(cnt)
-#         cx, cy = x + w / 2, y + h / 2
-
-#         if (abs(h * w)**2) > cv2.getTrackbarPos('Box filter', 'Trackbars'):
-
-#             cv2.rectangle(frame, (x, y), (x + w, y + h), [0, 0, 255], 2)
-#             print "******tennis ball detected!!***********"
-
-#     if cv2.getTrackbarPos('Caliberate', 'Trackbars') == 1:
-#         cv2.imshow('Output', thrImg)
-#     else:
-#         cv2.imshow('Output', frame)
-
-#     # cv2.waitKey(50)
-#     if cv2.waitKey(10) & 0xFF == ord('q'):
-#         break
-
-# cv2.destroyAllWindows()
-# c.release()
-
-# What would I need to pass to the controller when implementing in ROS for honing?
-# - bool: ballFound
-# - rectangle size
-# - hsv after filtering
-
-
-
-
-
 def main(args):
+    print("got to mains###########################################")
 
-    ic = image_converter()
-    rospy.init_node('image_converter', anonymous=True)
+    rospy.init_node('gate_detector', anonymous=True)
+    gd = gate_detector()
     try:
         rospy.spin()
     except KeyBoardInterrupt:
         print("Shutting down")
-    cv2.destroyAllWindows()
+#cv2.destroyAllWindows()
 
 if __name__ == '__main__':
+
     main(sys.argv)
