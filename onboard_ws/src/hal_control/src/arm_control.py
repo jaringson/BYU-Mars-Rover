670478bb508dc67c74fb5b35b02402ea5ad10e89
dwes7 <dwestley.1992@gmail.com>
2017-02-02 16:43:28 +0000

    Onboard builds and runs jetson on rover

---
+++ onboard_ws/src/hal_control/src/arm_control.py
@@ -0,0 +1,493 @@
+#!/usr/bin/env python
+
+import rospy, math
+#from ctypes import c_ushort
+from rover_msgs.msg import ArmState
+from sensor_msgs.msg import Joy, JointState
+from geometry_msgs.msg import Pose
+from std_msgs.msg import String,Float32MultiArray,UInt16MultiArray, Header, Int8
+import time
+import numpy as np
+from urdf_parser_py.urdf import URDF
+#from pykdl_utils.kdl_parser import kdl_tree_from_urdf_model
+#from pykdl_utils.kdl_kinematics import KDLKinematics
+import random
+import tf
+import tf.transformations as tr
+
+class Arm_XBOX():
+    def __init__(self):
+    # Variables
+        self.joy = Joy()
+        self.state = ArmState()
+        self.joints = JointState()
+        #self.joints_cart = Float32MultiArray()
+        self.pose_current = Pose()
+        self.pose_cmd = Pose()
+        self.grip = 0
+        
+        # Initialize state; default = JointControl & Medium
+        self.state.mode = 'JointControl' # 'JointControl', 'IK Arm - Base,Tool', 'IK Arm - Tool,Tool'
+        self.state.speed = 'Slow' # Slow, Med, Fast
+        self.state.kill = False
+
+        # Initialize joints = instance of JointState
+        self.joints.header = Header()
+        self.joints.header.stamp = rospy.Time.now()
+        self.joints.name = ['joint1', 'joint2', 'joint3', 'joint4', 'joint5', 'joint6']
+        self.joints.position = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+        self.joints.velocity = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+        self.joints.effort = []
+
+        # Initialize FK
+        self.init_ik = True
+        self.pose_current.position.x = 0.0
+        self.pose_current.position.y = 0.0
+        self.pose_current.position.z = 0.0
+
+    # Publishers and Subscribers
+
+    	# Subscribe to /joy_arm /pose_cmd
+        self.sub_joy = rospy.Subscriber('/joy_arm', Joy, self.joyCallback)
+        self.sub_pose_cmd= rospy.Subscriber('/pose_cmd', Pose, self.ikPoseCallback)
+        self.sub_joint_cmd_ik = rospy.Subscriber('/joint_cmd_ik',JointState, self.ikjointCallback)
+	
+        # Publish /arm_state_cmd; /joint_cmd; /grip; /joint_cart_cmd
+        self.pub_state = rospy.Publisher('/arm_state_cmd', ArmState, queue_size = 10)
+        self.pub_joints = rospy.Publisher('/joint_cmd', JointState, queue_size = 10)
+        self.pub_joint_ik = rospy.Publisher('/joint_ik', JointState, queue_size = 10)
+        self.pub_pose_ik = rospy.Publisher('/pose_ik', Pose, queue_size = 10)
+        self.pub_grip = rospy.Publisher('/grip', Int8, queue_size = 10)
+       
+    ##### Callbacks ###########
+
+    def joyCallback(self,msg):
+        self.joy=msg
+        if self.joy.buttons[9] == 1:
+            pass
+            # if self.check==False:            
+            #     self.check=True
+            # else:
+            #     self.check=False
+
+    def ikPoseCallback(self,msg):
+        self.pose_current = msg
+
+    def ikjointCallback(self, msg):
+        self.joints.position = msg.position
+        self.pub_joints.publish(self.joints)
+
+    # Functions
+    def check_method(self):
+        # Check to see whether driving or using arm and return case
+        # [A, B, X, Y] = buttons[0, 1, 2, 3]
+        y = self.joy.buttons[3] # toggle between modes
+        home = self.joy.buttons[8]
+        
+        # if y == 1: # UNCOMMENT THIS TO SWITCH BETWEEN MODES
+        #     if self.state.mode == 'JointControl':
+        #         self.state.mode = 'IK Arm - Base,Tool'
+        #     elif self.state.mode == 'IK Arm - Base,Tool':
+        #         self.state.mode = 'IK Arm - Tool,Tool'
+        #     else:
+        #         self.state.mode = 'JointControl'
+            # time.sleep(.25)
+            # rospy.loginfo(self.state.mode)
+            
+        # Implement Kill Switch
+        if home == 1:
+            if self.state.kill == False:
+                self.state.kill  = True
+            else:
+                self.state.kill  = False
+            time.sleep(.25)
+        
+        # Publish state commands
+        self.pub_state.publish(self.state)
+
+    def speed_check(self):
+    	# toggle between arm speeds
+        rb = self.joy.buttons[5]
+        if rb == 1:
+            if self.state.speed == 'Slow':
+                self.state.speed = 'Med'
+            elif self.state.speed == 'Med':
+                self.state.speed = 'Fast'
+            elif self.state.speed == 'Fast':
+                self.state.speed = 'Slow'
+            time.sleep(.25)
+
+    def gripper(self):
+        rt = (1 - self.joy.axes[5])/2.0
+        lt = (1 - self.joy.axes[2])/2.0
+        
+        threshold = 0.1
+        
+        if rt >= threshold: # open
+            self.grip = rt*100
+        elif lt >= threshold: # close
+            self.grip = -lt*100
+        else:
+            self.grip = 0
+
+    # ==========================================================================
+    # INVERSE KINEMATICS CONTROL Position = Base Frame; Orientation = End effector frame
+    # ==========================================================================
+    def arm_IK_base_tool(self):
+
+        # read in & initialize position of arm
+    	# if first time
+        if self.init_ik:
+            # Publish current joint position
+            self.pub_joint_ik.publish(self.joints)
+            time.sleep(.25)
+            self.pose_cmd = self.pose_current
+            self.init_ik = False
+        # FK on last commanded angles
+        
+    	###### change pose with Xbox
+        # Speed Check
+        self.speed_check()
+        
+        # Set corresponding rate
+        if self.state.speed == 'Fast':
+            MAX_RATE = .01
+        elif self.state.speed == 'Med':
+            MAX_RATE = .001
+        elif self.state.speed == 'Slow':
+            MAX_RATE = .0001
+            
+        ANGLE_RATE = 5
+        
+        # Calculate how to command arm (position control)
+        DEADZONE = 0.1
+
+        # Set axes
+        left_joy_up = self.joy.axes[1]
+        left_joy_right = self.joy.axes[0]
+        right_joy_up = self.joy.axes[4]
+        right_joy_right = self.joy.axes[3]
+        hat_up = self.joy.axes[7]
+        hat_right = self.joy.axes[6]
+        
+        # make array of axes
+        axes = [left_joy_right, left_joy_up, hat_up,
+            hat_right, right_joy_up, right_joy_right]
+        
+        # Set axis to zero in deadzone
+        for i in range(0,len(axes)):
+            if abs(axes[i])<DEADZONE:
+                axes[i] = 0
+                
+        # update pose_cmd with result from IK Node
+        self.pose_cmd = self.pose_current
+        
+        # Update Cartesian Positions
+        curRot = self.posemsg_to_rotmatrix(self.pose_cmd)
+        origin, xaxis, yaxis, zaxis = (0, 0, 0), (1, 0, 0), (0, 1, 0), (0, 0, 1)
+        
+        self.pose_cmd.position.x += axes[0]*MAX_RATE
+        self.pose_cmd.position.y -= axes[1]*MAX_RATE
+        self.pose_cmd.position.z += axes[4]*MAX_RATE
+        alpha = axes[2]*MAX_RATE*ANGLE_RATE
+        beta = axes[5]*MAX_RATE*ANGLE_RATE
+        gamma = axes[3]*MAX_RATE*ANGLE_RATE
+        Rx = tf.transformations.rotation_matrix(alpha, xaxis)
+        Ry = tf.transformations.rotation_matrix(beta,  yaxis)
+        Rz = tf.transformations.rotation_matrix(gamma, zaxis)
+        newRot = tf.transformations.concatenate_matrices(curRot,Rx,Ry,Rz)
+        quat = tf.transformations.quaternion_from_matrix(newRot)
+
+        self.pose_cmd.orientation.x = quat[0]
+        self.pose_cmd.orientation.y = quat[1]
+        self.pose_cmd.orientation.z = quat[2]
+        self.pose_cmd.orientation.w = quat[3]
+        
+       # print self.pose_cmd.position.x, -axes[0]*MAX_RATE, left_joy_right
+        
+    	# send pose to IK
+        self.pub_pose_ik.publish(self.pose_cmd)
+        
+        #print self.pose_cmd
+
+    # ==========================================================================
+    # INVERSE KINEMATICS CONTROL 2 Position = End Effector Frame; Orientation = End effector frame
+    # ==========================================================================
+    def arm_IK_tool_tool(self):
+
+        # read in & initialize position of arm
+        # if first time
+        if self.init_ik:
+            # Publish current joint position
+            self.pub_joint_ik.publish(self.joints)
+            time.sleep(.25)
+            self.pose_cmd = self.pose_current
+            self.init_ik = False
+        # FK on last commanded angles
+        
+        ###### change pose with Xbox
+        # Speed Check
+        self.speed_check()
+        
+        # Set corresponding rate
+        if self.state.speed == 'Fast':
+            MAX_RATE = .01
+        elif self.state.speed == 'Med':
+            MAX_RATE = .001
+        elif self.state.speed == 'Slow':
+            MAX_RATE = .0001
+            
+        ANGLE_RATE = 5
+        
+        # Calculate how to command arm (position control)
+        DEADZONE = 0.1
+
+        # Set axes
+        left_joy_up = self.joy.axes[1]
+        left_joy_right = self.joy.axes[0]
+        right_joy_up = self.joy.axes[4]
+        right_joy_right = self.joy.axes[3]
+        hat_up = self.joy.axes[7]
+        hat_right = self.joy.axes[6]
+        
+        # make array of axes
+        axes = [left_joy_right, left_joy_up, hat_up,
+            hat_right, right_joy_up, right_joy_right]
+        
+        # Set axis to zero in deadzone
+        for i in range(0,len(axes)):
+            if abs(axes[i])<DEADZONE:
+                axes[i] = 0
+                
+        # update pose_cmd with result from IK Node
+        self.pose_cmd = self.pose_current
+
+        ### Update Cartesian Position
+
+        # Get Current Transformation to End Effector Tip
+        curRot = self.posemsg_to_rotmatrix(self.pose_cmd)
+        T = self.posemsg_to_matrix(self.pose_cmd)
+        origin, xaxis, yaxis, zaxis = (0, 0, 0), (1, 0, 0), (0, 1, 0), (0, 0, 1)
+        
+        # x,y,z movement in tool frame
+        x_mvnt = axes[0]*MAX_RATE
+        y_mvnt = -axes[1]*MAX_RATE
+        z_mvnt = axes[4]*MAX_RATE
+        mvnt = np.matrix([x_mvnt, y_mvnt, z_mvnt])
+        dTool = tr.translation_matrix(mvnt)
+        dBase = tr.concatenate_matrices(T, dTool)
+	
+        self.pose_cmd.position.x = dBase.item(0, 3)
+        self.pose_cmd.position.y = dBase.item(1, 3)
+        self.pose_cmd.position.z = dBase.item(2, 3)
+
+        alpha = axes[2]*MAX_RATE*ANGLE_RATE
+        beta = axes[5]*MAX_RATE*ANGLE_RATE
+        gamma = axes[3]*MAX_RATE*ANGLE_RATE
+        Rx = tr.rotation_matrix(alpha, xaxis)
+        Ry = tr.rotation_matrix(beta,  yaxis)
+        Rz = tr.rotation_matrix(gamma, zaxis)
+        newRot = tr.concatenate_matrices(curRot, Rx, Ry, Rz)
+        quat = tr.quaternion_from_matrix(newRot)
+
+        self.pose_cmd.orientation.x = quat[0]
+        self.pose_cmd.orientation.y = quat[1]
+        self.pose_cmd.orientation.z = quat[2]
+        self.pose_cmd.orientation.w = quat[3]
+        
+       # print self.pose_cmd.position.x, -axes[0]*MAX_RATE, left_joy_right
+        
+        # send pose to IK
+        self.pub_pose_ik.publish(self.pose_cmd)
+        
+        #print self.pose_cmd
+
+    def posemsg_to_matrix(self,posemsg):
+        R = self.posemsg_to_rotmatrix(posemsg)
+        T = self.posemsg_to_transmatrix(posemsg)
+        H = tr.concatenate_matrices(T,R)
+        return H
+    
+    def posemsg_to_rotmatrix(self,posemsg):
+        quaternion = (
+            posemsg.orientation.x,
+            posemsg.orientation.y,
+            posemsg.orientation.z,
+            posemsg.orientation.w)
+        H = tr.quaternion_matrix(quaternion)
+        return H
+        
+    def posemsg_to_transmatrix(self,posemsg):
+	    trans = np.matrix([posemsg.position.x,posemsg.position.y,posemsg.position.z])
+	    H = tr.translation_matrix(trans)
+	    return H
+	    
+    
+        
+        
+
+    # ==========================================================================
+    # Xbox Arm Control ===============================================
+    # ==========================================================================
+    def joint_cmd(self):
+        
+        # Speed Check
+        self.speed_check()
+
+        # Set corresponding rate
+        if self.state.speed == 'Fast':
+        	MAX_RATE = 2*np.pi/180.0
+        elif self.state.speed == 'Med':
+        	MAX_RATE = 1*np.pi/180.0
+        elif self.state.speed == 'Slow':
+        	MAX_RATE = 0.5*np.pi/180.0
+
+        # Calculate how to command arm (position control)
+        DEADZONE = 0.1
+        
+        # Set axes
+        left_joy_up = self.joy.axes[1]
+        left_joy_right = self.joy.axes[0]
+        right_joy_up = self.joy.axes[4]
+        right_joy_right = self.joy.axes[3]
+        hat_up = self.joy.axes[7]
+        hat_right = self.joy.axes[6]
+        
+        # make array of axes
+        axes = [left_joy_right, left_joy_up, hat_up,
+            hat_right, right_joy_up, right_joy_right]
+        
+        # Set axis to zero in deadzone
+        for i in range(0,len(axes)):
+            if abs(axes[i])<DEADZONE:
+                axes[i] = 0
+                
+        # Update joint angles
+        for i in range(0,6):
+            self.joints.position[i] += axes[i]*MAX_RATE
+        
+        # Set joint angle limits
+        for i in range(0,len(self.joints.position)):
+            if self.joints.position[i] > np.pi:
+                self.joints.position[i] = np.pi
+            elif self.joints.position[i] < -np.pi:
+                self.joints.position[i] = -np.pi
+
+        self.joints.header.stamp = rospy.Time.now()
+        self.joints.header.frame_id = 'JointControl'                
+
+        # Gripper
+        self.gripper()
+
+        # # Shovel
+        # if self.joy.axes[2] < 0:
+        #     self.cmd.shovel = self.cmd.shovel-10.0
+        #     if self.cmd.shovel < 1000:
+        #         self.cmd.shovel = 1000
+        # elif self.joy.axes[5] < 0:
+        #     self.cmd.shovel = self.cmd.shovel+10.0
+        #     if self.cmd.shovel > 2000:
+        #         self.cmd.shovel = 2000
+        
+        # set flag so IK knows must init when entered again
+        self.init_ik = True
+
+        # Publish arm commands
+        self.pub_joints.publish(self.joints)
+        self.pub_joint_ik.publish(self.joints)
+
+
+#     # ==========================================================================
+#     # Velocity Arm Control ===============================================
+#     # ==========================================================================
+#     def vel_cmd(self):
+        
+#         # Speed Check
+#         self.speed_check()
+
+#         # Set corresponding rate
+#         if self.state.speed == 'Fast':
+#         	MAX_RATE = 100
+#         elif self.state.speed == 'Med':
+#         	MAX_RATE = 75
+#         elif self.state.speed == 'Slow':
+#         	MAX_RATE = 50
+
+#         # Calculate how to command arm (position control)
+#         DEADZONE = 0.1
+        
+#         # Set axes
+#         left_joy_up = self.joy.axes[1]
+#         left_joy_right = self.joy.axes[0]
+#         right_joy_up = self.joy.axes[4]
+#         right_joy_right = self.joy.axes[3]
+#         hat_up = self.joy.axes[7]
+#         hat_right = self.joy.axes[6]
+        
+#         # make array of axes
+#         axes = [left_joy_right, left_joy_up, hat_up,
+#             hat_right, right_joy_up, right_joy_right]
+        
+#         # Set axis to zero in deadzone
+#         for i in range(0,len(axes)):
+#             if abs(axes[i])<DEADZONE:
+#                 axes[i] = 0
+                
+#         # Update joint angles
+#         for i in range(0,6):
+#             self.joints.velocity[i] = axes[i]*MAX_RATE
+        
+# #        # Set joint angle limits
+# #        for i in range(0,len(self.joints.position)):
+# #            if self.joints.position[i] > np.pi:
+# #                self.joints.position[i] = np.pi
+# #            elif self.joints.position[i] < -np.pi:
+# #                self.joints.position[i] = -np.pi
+
+#         self.joints.header.stamp = rospy.Time.now()
+#         self.joints.header.frame_id = 'VelControl'        
+
+#         # Publish arm commands
+#         self.pub_joints.publish(self.joints)
+
+    # ==========================================================================
+    # Main ===============================================
+    # ==========================================================================
+if __name__ == '__main__':
+    # init ROS node
+    rospy.init_node('xbox_arm_control')
+    
+    # set rate
+    hz = 10.0#60.0
+    rate = rospy.Rate(hz)
+
+    # init Arm_xbox object
+    xbox = Arm_XBOX()
+    
+    # Loop
+    while not rospy.is_shutdown():
+
+    	# Start when Xbox controller recognized
+        if len(xbox.joy.buttons) > 0:
+            
+            # every time check toggle of state
+            xbox.check_method()
+
+            # check for kill switch (True = Killed)
+            if xbox.state.kill  == False:
+
+	            # call appropriate function for state
+	            # defaults to JointControl
+	            if xbox.state.mode == 'JointControl':
+	                xbox.joint_cmd()
+	            elif xbox.state.mode == 'IK Arm - Base,Tool':
+	                xbox.arm_IK_base_tool()
+	            elif xbox.state.mode == 'IK Arm - Tool,Tool':
+	                xbox.arm_IK_tool_tool()
+	            else:
+	                xbox.joint_cmd()
+        #else:
+         #   xbox.pub_joints.publish(xbox.joints)
+
+        rate.sleep()
+
---
+++ onboard_ws/src/psoc/src/onboard_psoc.py
@@ -0,0 +1,227 @@
+#!/usr/bin/env python
+
+import serial, rospy, struct
+from rover_msgs.msg import Drive, RoverState, PSOC # Pololu, SciFeedback
+from sensor_msgs.msg import JointState
+from std_msgs.msg import ByteMultiArray, Int8
+import re
+import numpy as np
+
+class PSOC_class():
+
+	def __init__(self):
+		# message:
+		# 0xEA, left-wheel-low, left-wheel-high, right-wheel-low,
+		#   right-wheel-high,
+		# cam-pan-low, cam-pan-high, cam-tilt-low, cam-tilt-high, cam-sel,
+		# turret-low, turret-high, shoulder-low, shoulder-high, elbow-low,
+		# elbow-high, forearm-low, forearm-high, 0x0, 0x0, 0x0, 0x0, hand-low,
+		# hand-high, chutes, shovel-low, shovel-high
+		#
+		# total number of bytes = 27
+		self.msg = ByteMultiArray()
+		self.msg.data.append(0)
+		self.msg.data.append(0)
+		self.msg.data.append(0)
+		self.msg.data.append(0)
+		self.msg.data.append(0)
+		self.msg.data.append(0)
+		self.msg.data.append(0)
+		self.msg.data.append(0)
+		self.msg.data.append(0)
+		self.msg.data.append(0)
+		self.msg.data.append(0)
+		self.msg.data.append(0)
+		self.msg.data.append(0)
+		self.msg.data.append(0)
+		self.msg.data.append(0)
+		self.msg.data.append(0)
+		self.msg.data.append(0)
+		self.msg.data.append(0)
+		self.msg.data.append(0)
+		self.msg.data.append(0)
+		self.msg.data.append(0)
+		self.msg.data.append(0)
+		self.msg.data.append(0)
+		self.msg.data.append(0)
+		self.msg.data.append(0)
+		self.msg.data.append(0)
+		self.msg.data.append(0)
+
+		# Init PSOC Message
+		self.psoc = PSOC()
+
+		# initialize Drive msg stuff
+		self.psoc.lw = np.uint16(1500)
+		self.psoc.rw = np.uint16(1500)
+
+		# initialize RoverState stuff
+		self.psoc.pan = np.uint16(1500)
+		self.psoc.tilt = np.uint16(1500)
+		self.psoc.camnum = np.uint16(0)
+		self.psoc.chutes = np.uint16(0)
+
+		# initialize JointState Stuff
+		self.psoc.q1 = np.uint16(2046)
+		self.psoc.q2 = np.uint16(2046)
+		self.psoc.q3 = np.uint16(2046)
+		self.psoc.q4 = np.uint16(2046)
+		self.psoc.q5 = np.uint16(2046)
+		self.psoc.q6 = np.uint16(2046)
+
+		# initialize Grip Stuff
+		self.psoc.grip = np.uint16(0)
+
+		# feedback: 0xE3, turret-low, turret-high, shoulder-low, shoulder-high,
+		# elbow-low, elbow-high, forearm-low, forearm-high, temperature-low,
+		# temperature-high, humidity-low, humidity-high
+		self.feedback = ByteMultiArray()
+		#self.science_data = SciFeedback()
+		#self.arm_feedback = Pololu()
+
+		# initialize serial port here
+		self.ser = serial.Serial('/dev/ttyUSB2', 57600, timeout = 1)
+		# if self.ser.is_open():
+		# 	self.ser.close()
+
+		# initialize subscribers
+		self.sub_drive = rospy.Subscriber('/drive_cmd', Drive, self.drive_callback)
+		self.sub_state = rospy.Subscriber('/rover_state_cmd', RoverState, self.state_callback)
+		self.sub_joint = rospy.Subscriber('/joint_cmd', JointState, self.joint_callback)
+		self.sub_grip = rospy.Subscriber('/grip', Int8, self.grip_callback)        
+
+        # initialize publishers
+		self.pub_psoc = rospy.Publisher('/psoc_out2', PSOC, queue_size=1)
+        #self.pub_arm = rospy.Publisher('/arm_feedback', Pololu, queue_size=1)
+
+    # Callback
+    
+    # Drive callback
+
+	def drive_callback(self, drive):
+    # Lastyear .lw & .rw were from 1000 to 2000, 1500 is no movement
+    # This year .l1 & .rw are from -100 to 100
+
+		lw_temp = 1500 + (5*drive.lw)
+		rw_temp = 1500 + (5*-drive.rw)
+
+		self.psoc.lw = np.uint16(lw_temp)
+		self.psoc.rw = np.uint16(rw_temp)
+
+		self.set_rover_cmd()
+
+    # Rover State Callback
+    #     
+	def state_callback(self, state):
+		# self.state.pan = state.pan
+		# self.state.tilt = state.tilt 
+		self.psoc.pan = np.uint16(1500)
+		self.psoc.tilt = np.uint16(1500) 
+		self.psoc.camnum = np.uint16(state.camnum)
+		self.psoc.chutes = np.uint16(state.chutes)
+
+		self.set_rover_cmd()
+
+    # Joint Callback
+    # Last year = values from 0 to 4092 for each joint, representing commanded angle
+    # Giving radian angle for each joint (we can give you velocities scalars from -100 to 100 if you want)
+	def joint_callback(self, joint):
+		pos_temp = [0, 0, 0, 0, 0, 0]
+
+		# assume all joint go from -pi to pi
+		# 0 = -pi; 4092 = pi
+		pos_temp[0] = 2046 + 2046*(joint.position[0]/np.pi)
+		pos_temp[1] = 2046 + 2046*(joint.position[1]/np.pi)
+		pos_temp[2] = 2046 + 2046*(joint.position[2]/np.pi)
+		pos_temp[3] = 2046 + 2046*(joint.position[3]/np.pi)
+		pos_temp[4] = 2046 + 2046*(joint.position[4]/np.pi)
+		pos_temp[5] = 2046 + 2046*(joint.position[5]/np.pi)
+
+		self.psoc.q1 = np.uint16(pos_temp[0])
+		self.psoc.q2 = np.uint16(pos_temp[1])
+		self.psoc.q3 = np.uint16(pos_temp[2])
+		self.psoc.q4 = np.uint16(pos_temp[3])
+		self.psoc.q5 = np.uint16(pos_temp[4])
+		self.psoc.q6 = np.uint16(pos_temp[5])
+
+		self.set_rover_cmd()
+
+	def grip_callback(self, grip):
+		self.psoc.grip = np.uint16(grip)
+
+		self.set_rover_cmd()
+    
+	def set_rover_cmd(self):
+
+		self.msg.data[0] = 0xEA
+		self.msg.data[1] = self.psoc.lw & 0xff
+		self.msg.data[2] = (self.psoc.lw & 0xff00) >> 8
+		self.msg.data[3] = self.psoc.rw & 0xff
+		self.msg.data[4] = (self.psoc.rw & 0xff00) >> 8
+		self.msg.data[5] = self.psoc.pan & 0xff
+		self.msg.data[6] = (self.psoc.pan & 0xff00) >> 8
+		self.msg.data[7] = self.psoc.tilt & 0xff
+		self.msg.data[8] = (self.psoc.tilt & 0xff00) >> 8
+		self.msg.data[9] = self.psoc.camnum
+		self.msg.data[10] = self.psoc.q1 & 0xff
+		self.msg.data[11] = (self.psoc.q1 & 0xff00) >> 8
+		self.msg.data[12] = self.psoc.q2 & 0xff
+		self.msg.data[13] = (self.psoc.q2 & 0xff00) >> 8
+		self.msg.data[14] = self.psoc.q3 & 0xff
+		self.msg.data[15] = (self.psoc.q3 & 0xff00) >> 8
+		self.msg.data[16] = self.psoc.q4 & 0xff
+		self.msg.data[17] = (self.psoc.q4 & 0xff00) >> 8
+		self.msg.data[18] = self.psoc.q5 & 0xff
+		self.msg.data[19] = 0
+		self.msg.data[20] = 0
+		self.msg.data[21] = 0
+		self.msg.data[22] = self.psoc.grip & 0xff
+		self.msg.data[23] = (self.psoc.grip & 0xff00) >> 8
+		self.msg.data[24] = self.psoc.chutes
+		self.msg.data[25] = np.uint16(1500) & 0xff #cmd.psoc.shovel & 0xff
+		self.msg.data[26] = (np.uint16(1500) & 0xff00) >> 8#(cmd.psoc.shovel & 0xff00) >> 8
+
+		# print 'Ser open?'
+		# print self.ser.isOpen()
+
+		string = ''
+		for i in self.msg.data:
+			string += struct.pack('!B',i)
+		bwrite = self.ser.write(string)
+		# print bwrite
+
+		# publish values just written to psoc
+		self.pub_psoc.publish(self.psoc)
+
+	def remove_non_ascii(self,text):
+		return ''.join([i if ord(i)<128 else ' ' for i in text])
+
+	def read_feedback(self):
+		pass
+		# # read feedback from psoc here if at least 13 bytes have been received
+		# if self.ser.inWaiting() >= 13:
+		# 	while self.ser.inWaiting() > 0:
+		# 		if self.ser.read(1).encode('hex') in 'e3':
+		# 			self.arm_feedback.q1 = ord(self.ser.read(1).decode('string_escape')) | (ord(self.ser.read(1).decode('string_escape')) << 8)
+		# 			self.arm_feedback.q2 = ord(self.ser.read(1).decode('string_escape')) | (ord(self.ser.read(1).decode('string_escape')) << 8)
+		# 			self.arm_feedback.q3 = ord(self.ser.read(1).decode('string_escape')) | (ord(self.ser.read(1).decode('string_escape')) << 8)
+		# 			self.arm_feedback.q4 = ord(self.ser.read(1).decode('string_escape')) | (ord(self.ser.read(1).decode('string_escape')) << 8)
+		# 			self.science_data.temp = ord(self.ser.read(1).decode('string_escape')) | (ord(self.ser.read(1).decode('string_escape')) << 8)
+		# 			self.science_data.humidity = ord(self.ser.read(1).decode('string_escape')) | (ord(self.ser.read(1).decode('string_escape')) << 8)
+		# 			#		    self.pub_arm.publish(self.arm_feedback)
+		# 			#		    self.pub_sci.publish(self.science_data)
+
+
+if __name__ == '__main__':
+	rospy.init_node('psoc_node', anonymous=True)
+	hz = 60.0
+	rate = rospy.Rate(hz)
+	# call the constructor
+	psoc = PSOC_class()
+
+	while not rospy.is_shutdown():
+		# psoc.set_rover_cmd()
+		# psoc.read_feedback()
+		rate.sleep()
+
+	psoc.ser.close()
