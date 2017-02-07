76f8ef97f437f36c4c3c401a211f7e9249b2a01c
dwes7 <dwestley.1992@gmail.com>
2017-02-07 10:17:16 -0700

    started tested hsv calibration node. Not finished yet

--- rover_ws/src/rover_hub/src/rover_hub_node.cpp
+++ rover_ws/src/rover_hub/src/rover_hub_node.cpp
@@ -1,26 +1,26 @@
 #include "ros/ros.h"
-#include "rover_hub.h"
+#include "rover_hub/rover_hub.h"
 
 int main(int argc, char **argv)
 {
-    ros::init(argc, argv, "rover_hub_node");
+//     ros::init(argc, argv, "rover_hub_node");
 
-//    ros::NodeHandle nh;
-    ros::NodeHandle nh_private("~");
-    int rate;
-    nh_private.param<int>("rate", rate, 100);
+// //    ros::NodeHandle nh;
+//     ros::NodeHandle nh_private("~");
+//     int rate;
+//     nh_private.param<int>("rate", rate, 100);
 
-    rover_hub::Rover_hub rh;
-    ros::Rate loop_rate(rate);
-    while(ros::ok())
-    {
-        rh;
-        ros::spinOnce();
-//        ro.publish_image();
+//     rover_hub::Rover_hub rh;
+//     ros::Rate loop_rate(rate);
+//     while(ros::ok())
+//     {
+//         rh;
+//         ros::spinOnce();
+// //        ro.publish_image();
 
-        loop_rate.sleep();
-        //ROS_WARN_STREAM("Spinning");
-    }
+//         loop_rate.sleep();
+//         //ROS_WARN_STREAM("Spinning");
+//     }
 
 //    ros::spin();
     return 0;
