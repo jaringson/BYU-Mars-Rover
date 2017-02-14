16cad31a8e5766e11880d8fbf96fb726ded350f8
michael <michaeldavidfarrell@gmail.com>
2017-02-14 09:04:03 -0700

    Update installer to kinetic

--- scripts/install_packages.sh
+++ scripts/install_packages.sh
@@ -3,15 +3,15 @@
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
 sudo apt-get update
-sudo apt-get install ros-indigo-desktop-full
+sudo apt-get install ros-kinetic-desktop-full
 sudo rosdep init
 rosdep update
-echo "source /opt/ros/indigo/setup.bash" >> ~/.bashrc
+echo "source /opt/ros/kinetic/setup.bash" >> ~/.bashrc
 source ~/.bashrc
 sudo apt-get install python-rosinstall
 
 # Install Dependencies
-sudo apt-get install ros-indigo-joy
-sudo apt-get install ros-indigo-urdfdom-py
-sudo apt-get install ros-indigo-trac-ik
-sudo apt-get install ros-indigo-moveit-full
+sudo apt-get install ros-kinetic-joy
+sudo apt-get install ros-kinetic-urdfdom-py
+sudo apt-get install ros-kinetic-trac-ik
+sudo apt-get install ros-kinetic-moveit-full
