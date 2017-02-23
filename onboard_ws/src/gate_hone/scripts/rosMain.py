1c9a41f95961e0915e18b69fd0f6843eea8b1883
Peter <pschleede@gmail.com>
2017-02-23 10:06:34 -0700

    added conditional in callback on gate honing

--- onboard_ws/src/gate_hone/scripts/rosMain.py
+++ onboard_ws/src/gate_hone/scripts/rosMain.py
@@ -11,38 +11,42 @@ import controllerPID as ctrl
 # When a new message is sent to the topic '/gate_info',
 # this function will run.
 def callback(data):
-    #message contains coordinates of ball centroid and image dimensions
-    x = data.coords[0]
-    image_width = data.image_size[1]
+    #only do stuff if gate is detected
+    if (data.gate_detected):
 
-    #states are [x1-x2,(x1-x2)dot].T, we care about x1-x2
-    states = x-(image_width-x)
+        '''
+        need another logic statement for if error_d1>something
+        if 1:
+            do all the turning stuff (xcontrol)
+        else:
+            go to the gate (zcontrol, based on box size)
+        '''
 
-    # Calculate time elapsed since last function call, s
-    global prev_time
-    now = rospy.Time.now()
-    dt = (now-prev_time).to_sec()
-    prev_time = now               # Update prev_time , time
-    global sim_time
-    sim_time = round(sim_time+dt,6)
+        #message contains coordinates of ball centroid and image dimensions
+        x = data.coords[0]
+        image_width = data.image_size[1]
 
-    # the desired reference will always be 0 because we want to center the ball
-    ref_input = 0
+        #states are [x1-x2,(x1-x2)dot].T, we care about x1-x2
+        states = x-(image_width-x)
 
-    u = ctrl.getForces(ref_input,states) # Calculate the forces
+        # Calculate time elapsed since last function call, s
+        global prev_time
+        now = rospy.Time.now()
+        dt = (now-prev_time).to_sec()
+        prev_time = now               # Update prev_time , time
+        global sim_time
+        sim_time = round(sim_time+dt,6)
 
+        # the desired reference will always be 0 because we want to center the ball
+        ref_input = 0
 
+        u = ctrl.getInputs(ref_input,states) # Calculate the forces
 
-    command.lw = -u
-    command.rw = u
-    command_pub.publish(command)
-
-
+        command.lw = -u
+        command.rw = u
+        command_pub.publish(command)
 
 if __name__ == '__main__':
-
-    #-----NOT NECESSARY
-    usr_input = Sliders() if SLIDERS else Signals()
 
     ctrl = ctrl()                         # Instantiate controllerPD class
 
@@ -51,11 +55,11 @@ if __name__ == '__main__':
 
     # Subscriber to topic '/gate_info' which contains the pixel location of the
     # gate, (along with size of the box?)
-    rospy.Subscriber('/gate_info', GateInfo, callback)
+    rospy.Subscriber('gate_detector/gate_info', GateInfo, callback)
 
     # Publisher to topic 'command'
     #-----PUBLISH TO ROVER COMMAND
-    command_pub = rospy.Publisher('SOMETHING',Dive,queue_size=5)
+    command_pub = rospy.Publisher('SOMETHING',Drive,queue_size=5)
 
     # Command Message object
     command=Drive()
