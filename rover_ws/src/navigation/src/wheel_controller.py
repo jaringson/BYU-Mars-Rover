2385e63c325ace3bde94306a8d82a46e9f72ea67
michael <michaeldavidfarrell@gmail.com>
2017-02-24 09:01:17 -0700

    Scale up output and fix saturation error

--- rover_ws/src/navigation/src/wheel_controller.py
+++ rover_ws/src/navigation/src/wheel_controller.py
@@ -8,7 +8,7 @@ class WheelControl:
         # Set Parameters
         self.L = 0.6096  # Distance between wheels (2 ft)
         self.R = 0.1524  # Radius of wheels
-        self.max_vel = 1  # Maximum turn rate, corresponds to 100 in /rover_msg/Drive. Radians per second
+        self.max_vel = 6.5617/75.0  # Maximum turn rate, corresponds to 100 in /rover_msg/Drive. Radians per second
 
         # init ROS node
         rospy.init_node('navigation')
@@ -29,24 +29,30 @@ class WheelControl:
     def cmd_vel_callback(self, msg):
         v = msg.linear.x
         w = msg.angular.z
-
+	
         vr = (2*v + w*self.L)/(2*self.R) # Turn rate of right wheel
         vl = (2*v - w*self.L)/(2*self.R) # Turn rate of left wheel
+	
+	
+	#print vr, vl
 
         rw = self.sat(vr/self.max_vel, 100)
         lw = self.sat(vl/self.max_vel, 100)
+	
+#	print rw, lw
 
         # Create Drive Message
         cmd = Drive()
         cmd.rw = rw
         cmd.lw = lw
         self.pub_drive.publish(cmd)
+#	print cmd
 
     @staticmethod
     def sat(x, lim):
         if x > lim:
             x = lim
-        elif x < lim:
+        elif x < -lim:
             x = -lim
         return x
 
