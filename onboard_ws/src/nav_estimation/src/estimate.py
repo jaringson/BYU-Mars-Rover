a36e457c7b8236459df1e5ecfb5c51d1ccc2d53d
michael <michaeldavidfarrell@gmail.com>
2017-03-08 07:58:00 -0700

    estimator needs to be tested, 2017 psoc started

--- onboard_ws/src/nav_estimation/src/estimate.py
+++ onboard_ws/src/nav_estimation/src/estimate.py
@@ -8,6 +8,7 @@ from sensor_msgs.msg import NavSatFix, Imu
 from geometry_msgs.msg import TwistStamped
 import time
 import numpy as np
+import tf
 
 
 class Estimator():
@@ -30,7 +31,11 @@ class Estimator():
 		self.estimate.Vg = 0
 		self.estimate.base_latitude = 0
 		self.estimate.base_longitude = 0
-		self.base_altitude = 0
+		self.estimate.base_altitude = 0
+
+		self.phat = 0
+		self.qhat = 0
+		self.rhat = 0
 
 		# GPS Init
 		self.gps_init = False
@@ -38,6 +43,8 @@ class Estimator():
 		# Init Kalman Filter stuff
 		self.N = 10 # Steps for Kalman Filter
 		self.xhat = np.zeros(3) # pn, pe, psi
+
+		self.L = np.zeros(3)
 		
 		self.P = np.identity(3)
 		self.P[0][0] = 0.03 # pn
@@ -56,8 +63,8 @@ class Estimator():
 		self.R[1][1] = sigma_h_gps**2 # pe
 		self.R[2][2] = 0.0001 # psi # FIXXX THISSSSSS
 
-		self.time_old = 0
-		self.time_new = 0
+		self.time_old = rospy.get_rostime()
+		self.time_new = rospy.get_rostime()
 
 		# Callback transfers
 		self.Vwhat = 0
@@ -73,7 +80,8 @@ class Estimator():
 		# GAINS
 		self.drive_gain = 0.003511
 		self.turn_gain = 0.004
-		self.alpha = 0.0
+
+		self.lpf_a = 0.0
 
 		# Constants
 		self.EARTH_RADIUS = 6378145.0
@@ -89,26 +97,60 @@ class Estimator():
 
 	# Functions
 	def gpsCallback(self, msg):
+		# print 'GPS Callback'
 		
 		# init GPS
 		if not self.gps_init:
+			# print 'GPS INIT'
 			self.gps_init = True
 			self.estimate.base_latitude = msg.latitude
 			self.estimate.base_longitude = msg.longitude
 			self.estimate.base_altitude = msg.altitude
+			self.time_old = rospy.get_rostime()
 		else:
+			# print 'GPS Input'
 			self.gps_n = self.EARTH_RADIUS*(msg.latitude-self.estimate.base_latitude)*np.pi/180.0
-			self.gps_e = self.EARTH_RADIUS*cos(self.base_latitude*np.pi/180.0)*(msg.longitude-self.base_longitude)*np.pi/180.0
-			self.gps_h = msg.altitude - self.base_altitude
+			self.gps_e = self.EARTH_RADIUS*cos(self.estimate.base_latitude*np.pi/180.0)*(msg.longitude-self.estimate.base_longitude)*np.pi/180.0
+			self.gps_h = msg.altitude - self.estimate.base_altitude
+
+			self.gps_new = True
+			self.update()
 	
 	def imuCallback(self, msg):
-		pass
+		
+		# Get time
+		self.time_new = rospy.get_rostime()
+		Ts = (self.time_new.secs - self.time_old.secs) + (self.time_new.nsecs - self.time_old.nsecs)/1e9
+		self.time_old = self.time_new
+
+		if (Ts > 0.5):
+			rospy.logwarn('Estimation Time Step was Too Big: %f', Ts)
+		else:
+
+			# Compute alpha for time step
+			alpha = exp(-self.lpf_a*Ts)
+
+			self.phat = alpha*self.phat + (1-alpha)*msg.angular_velocity.x
+			self.qhat = alpha*self.qhat + (1-alpha)*msg.angular_velocity.y
+			self.rhat = alpha*self.rhat + (1-alpha)*msg.angular_velocity.z
+
+			q = (msg.orientation.x, msg.orientation.y, msg.orientation.z, msg.orientation.w)
+			euler = tf.transformations.euler_from_quaternion(q)
+			self.estimate.phi = euler[0];
+			self.estimate.theta = euler[1];
+			self.estimate.psi = euler[2];
+
+			self.prediction(Ts)
+
+			if self.gps_init:
+				self.publish()
 
 	def driveCallback(self, msg):
 		self.Vwhat = (msg.lw + msg.rw)*self.drive_gain
 		self.omegahat = (msg.lw - msg.rw)*self.turn_gain
 
 	def prediction(self, Ts):
+		# print 'Prediction'
 		
 		for i in range(0, self.N):
 
@@ -126,13 +168,49 @@ class Estimator():
 
 
 	def update(self):
-		
+		# print 'Update'
+
 		# Init identity
 		I = np.identity(3)
 
+		if self.gps_new:
+
+			# Reset New measurement flag
+			self.gps_new = False
+
+			# GPS North
+			h = self.xhat[0] #pn
+			C = np.zeros(3)
+			C[0] = 1
+			denom = (self.R[0][0] + np.dot(np.dot(C.transpose(),self.P),C))
+			self.L = (np.dot(self.P,C))/denom
+			self.P = (I - np.dot(np.dot(self.L,C.transpose()),self.P))
+			self.xhat = self.xhat + self.L*(self.gps_n - h)
+			# print 'GPS N L:'
+			# print self.L
+
+			# # GPS East
+			h = self.xhat[1] #pe
+			C = np.zeros(3)
+			C[1] = 1
+			denom = (self.R[1][1] + np.dot(np.dot(C.transpose(),self.P),C))
+			self.L = (np.dot(self.P,C))/denom
+			self.P = (I - np.dot(np.dot(self.L,C.transpose()),self.P))
+			self.xhat = self.xhat + self.L*(self.gps_e - h)
+			# print 'GPS E L:'
+			# print self.L
+
+		# Error if estimates aren't realistic
+		if (self.xhat[0] > 2000 or self.xhat[0] < -2000 or self.xhat[1] < -2000 or self.xhat[1] > 2000):
+			rospy.logwarn('GPS Estimates Too Big Pn: %f, Pe: %f', self.xhat[0], self.xhat[1])
+
 	def publish(self):
-		self.prediction(0.05)
-		self.pub_state.publish()
+		self.estimate.position[0] = self.xhat[0]
+		self.estimate.position[1] = self.xhat[1]
+		self.estimate.position[2] = self.gps_h # not estimated
+		self.estimate.Vw = self.Vwhat # input
+		self.estimate.Vg = self.omegahat # omega is published as Vg for now
+		self.pub_state.publish(self.estimate)
 
 
 	# ==========================================================================
@@ -152,6 +230,7 @@ if __name__ == '__main__':
 	
 	# Loop
 	while not rospy.is_shutdown():
+
 		estimate.publish()
 		rate.sleep()
 
