ef6e17d0f47c5b28d8bfd0146ab3bdde132a94a2
dwes7 <dwestley.1992@gmail.com>
2017-04-03 15:22:42 +0000

    Jetson psoc push

--- onboard_ws/src/psoc/src/2017_psoc.py
+++ onboard_ws/src/psoc/src/2017_psoc.py
@@ -10,23 +10,24 @@ import numpy as np
 class PSOC_class():
 
 	def __init__(self):
-		# message:
-		# # 2017 Psoc Code
+		# 2017 Psoc Code
 		# Premble (1 Byte) (0)
 		# LW Speed (2 Bytes) (1,2)
 		# LW Direction (1 Byte) (3)
 		# RW Speed (2 Bytes) (4,5)
 		# RW Direction (1 Byte) (6)
-		# Turret/Plunge (2 Bytes) (7,8)
-		# Shoulder/Plate (2 Bytes) (9, 10)
-		# Elbow/Drill (2 Bytes) (11,12)
-		# Forearm/Elevator (2 Bytes) (13,14)
-		# Hand (1 Byte) (15)
-		# Chutes (1 Byte) (16)
-		# Total = 17 Bytes
+		# Turret (2 Bytes) (7,8)
+		# Shoulder/Elevator (2 Bytes) (9, 10)
+		# Elbow/Plunge (2 Bytes) (11,12)
+		# Forearm/Drill (2 Bytes) (13,14)
+		# Plate Shift (2 Bytes) (15,16)
+		# Hand (1 Byte) (17) # 0 - nothing, 1 2
+		# Chutes (1 Byte) (18)
+		# Total = 19 Bytes
 		#
-		# total number of bytes = 17
 		self.msg = ByteMultiArray()
+		self.msg.data.append(0)
+		self.msg.data.append(0)
 		self.msg.data.append(0)
 		self.msg.data.append(0)
 		self.msg.data.append(0)
@@ -49,21 +50,24 @@ class PSOC_class():
 		self.psoc = PSOC17()
 
 		# initialize Drive msg stuff
+		self.new_drive = 0
 		self.psoc.lw = np.uint16(0)
 		self.psoc.rw = np.uint16(0)
 		self.psoc.lwdirection = 1 # 1 if forward, 0 is backward
 		self.psoc.rwdirection = 1 # 1 if forward, 0 if backward
 
 		# initialize chutes stuff
-		self.psoc.chutes = np.uint16(0)
+		self.psoc.chutes = np.uint8(79) # Init byte message to '01001111'
 
 		# initialize JointState Stuff
+		self.arm_initialized = 0
 		self.psoc.q1 = np.uint16(2046)
 		self.psoc.q2 = np.uint16(2046)
 		self.psoc.q3 = np.uint16(2046)
 		self.psoc.q4 = np.uint16(2046)
 		self.psoc.q5 = np.uint16(2046)
 		self.psoc.q6 = np.uint16(2046)
+		self.psoc.plate = np.uint16(0)
 
 		# initialize Grip Stuff
 		self.psoc.grip = np.uint16(0)
@@ -89,7 +93,9 @@ class PSOC_class():
 
         # initialize publishers
 		self.pub_psoc = rospy.Publisher('/psoc_out', PSOC17, queue_size=1)
-        #self.pub_arm = rospy.Publisher('/arm_feedback', Pololu, queue_size=1)
+		print "Successfull"
+     
+   #self.pub_arm = rospy.Publisher('/arm_feedback', Pololu, queue_size=1)
 
     # Callback
     
@@ -114,16 +120,17 @@ class PSOC_class():
 		else:
 			self.psoc.rwdirection = 0
 
-		self.set_rover_cmd()
+		self.new_drive = 1
+
+		# self.set_rover_cmd()
 
     # Rover State Callback
     #     
 	def state_callback(self, state):
 		
-		# self.psoc.camnum = np.uint16(state.camnum)
 		self.psoc.chutes = np.uint16(state.chutes)
 
-		self.set_rover_cmd()
+		# self.set_rover_cmd()
 
     # Joint Callback
     # Last year = values from 0 to 4092 for each joint, representing commanded angle
@@ -147,37 +154,41 @@ class PSOC_class():
 		self.psoc.q5 = np.uint16(pos_temp[4])
 		self.psoc.q6 = np.uint16(pos_temp[5])
 
-		self.set_rover_cmd()
+		# self.set_rover_cmd()
 
     # Science Callback
 	def science_callback(self, msg):
 
 		# Science Uses the same pololus as the Arm:
 		# Here are which pololus correspond to which
-		# q1 = Turret
+		# q1 = 0 # not used for science
 		# q2 = elevator = Shoulder
 		# q3 = Plunge = Elbow
 		# q4 = Drill = Forearm
+		# plate = Science Plate Shift
 
-		self.psoc.q1 = np.uint16(msg.plunge)
-		self.psoc.q2 = np.uint16(msg.plate)
+		self.psoc.q2 = np.uint16(msg.elevator)
+		self.psoc.q3 = np.uint16(msg.plunge)
 
 		# CHECK WHAT TO SEND FOR THE DRILL WHEN ON/OFF
 		if msg.drill:
-			self.psoc.q3 = np.uint16(0)
+			self.psoc.q4 = np.uint16(4095)
 		elif not msg.drill:
-			self.psoc.q3 = np.uint16(2000)
-		self.psoc.q3 = msg.drill
-		self.psoc.q4 = np.uint16(msg.elevator)
+			self.psoc.q4 = np.uint16(2048)
+
+		self.psoc.plate = np.uint16(msg.plate)
+
+		self.psoc.q1 = np.uint16(0)
 		self.psoc.q5 = np.uint16(0)
 		self.psoc.q6 = np.uint16(0)
 
-		self.set_rover_cmd()
+		# self.set_rover_cmd()
 
 	def grip_callback(self, grip):
-		self.psoc.grip = np.uint16(grip)
 
-		self.set_rover_cmd()
+		self.psoc.grip = np.uint16(grip.data)
+
+		# self.set_rover_cmd()
     
 	def set_rover_cmd(self):
 
@@ -187,13 +198,18 @@ class PSOC_class():
 		# LW Direction (1 Byte) (3)
 		# RW Speed (2 Bytes) (4,5)
 		# RW Direction (1 Byte) (6)
-		# Turret/Plunge (2 Bytes) (7,8)
-		# Shoulder/Plate (2 Bytes) (9, 10)
-		# Elbow/Drill (2 Bytes) (11,12)
-		# Forearm/Elevator (2 Bytes) (13,14)
-		# Hand (1 Byte) (15) # 0 - nothing, 1 2
-		# Chutes (1 Byte) (16)
-		# Total = 17 Bytes
+		# Turret (2 Bytes) (7,8)
+		# Shoulder/Elevator (2 Bytes) (9, 10)
+		# Elbow/Plunge (2 Bytes) (11,12)
+		# Forearm/Drill (2 Bytes) (13,14)
+		# Plate Shift (2 Bytes) (15,16)
+		# Hand (1 Byte) (17) # 0 - nothing, 1 2
+		# Chutes (1 Byte) (18)
+		# Total = 19 Bytes
+
+		if self.new_drive == 0:
+			self.psoc.lw = 0
+			self.psoc.rw = 0
 
 		self.msg.data[0] = 0xEA
 		self.msg.data[1] = self.psoc.lw & 0xff
@@ -212,9 +228,11 @@ class PSOC_class():
 		self.msg.data[12] = (self.psoc.q3 & 0xff00) >> 8
 		self.msg.data[13] = self.psoc.q4 & 0xff
 		self.msg.data[14] = (self.psoc.q4 & 0xff00) >> 8
+		self.msg.data[15] = self.psoc.plate & 0xff
+		self.msg.data[16] = (self.psoc.plate & 0xff00) >> 8
 
-		self.msg.data[15] = self.psoc.grip
-		self.msg.data[16] = self.psoc.chutes
+		self.msg.data[17] = self.psoc.grip
+		self.msg.data[18] = self.psoc.chutes
 
 		# print 'Ser open?'
 		# print self.ser.isOpen()
@@ -225,6 +243,9 @@ class PSOC_class():
 		bwrite = self.ser.write(string)
 		# print bwrite
 
+		# reset new drive flag
+		self.new_drive = 0
+
 		# publish values just written to psoc
 		self.pub_psoc.publish(self.psoc)
 
@@ -232,19 +253,60 @@ class PSOC_class():
 		return ''.join([i if ord(i)<128 else ' ' for i in text])
 
 	def read_feedback(self):
-		pass
-		# # read feedback from psoc here if at least 13 bytes have been received
-		# if self.ser.inWaiting() >= 13:
-		# 	while self.ser.inWaiting() > 0:
-		# 		if self.ser.read(1).encode('hex') in 'e3':
-		# 			self.arm_feedback.q1 = ord(self.ser.read(1).decode('string_escape')) | (ord(self.ser.read(1).decode('string_escape')) << 8)
-		# 			self.arm_feedback.q2 = ord(self.ser.read(1).decode('string_escape')) | (ord(self.ser.read(1).decode('string_escape')) << 8)
-		# 			self.arm_feedback.q3 = ord(self.ser.read(1).decode('string_escape')) | (ord(self.ser.read(1).decode('string_escape')) << 8)
-		# 			self.arm_feedback.q4 = ord(self.ser.read(1).decode('string_escape')) | (ord(self.ser.read(1).decode('string_escape')) << 8)
-		# 			self.science_data.temp = ord(self.ser.read(1).decode('string_escape')) | (ord(self.ser.read(1).decode('string_escape')) << 8)
-		# 			self.science_data.humidity = ord(self.ser.read(1).decode('string_escape')) | (ord(self.ser.read(1).decode('string_escape')) << 8)
-		# 			#		    self.pub_arm.publish(self.arm_feedback)
-		# 			#		    self.pub_sci.publish(self.science_data)
+		# read feedback from psoc here if at least 13 bytes have been received
+		if self.ser.inWaiting() >= 15:
+			print "READ FEEDBACK!"
+			while self.ser.inWaiting() > 0:
+				if self.ser.read(1).encode('hex') in 'e3':
+					try:
+						self.q1_fback = ord(self.ser.read(1).decode('string_escape')) | (ord(self.ser.read(1).decode('string_escape')) << 8)
+						print self.q1_fback
+					except ValueError:
+						print "q1 Bad Feedback"
+
+					try:
+						self.q2_fback = ord(self.ser.read(1).decode('string_escape')) | (ord(self.ser.read(1).decode('string_escape')) << 8)
+						print self.q2_fback
+					except ValueError:
+						print "q2 Bad Feedback"
+
+					try:
+						self.q3_fback = ord(self.ser.read(1).decode('string_escape')) | (ord(self.ser.read(1).decode('string_escape')) << 8)
+						print self.q3_fback
+					except ValueError:
+						print "q3 Bad Feedback"
+
+					try:
+						self.q4_fback = ord(self.ser.read(1).decode('string_escape')) | (ord(self.ser.read(1).decode('string_escape')) << 8)
+						print self.q4_fback
+					except ValueError:
+						print "q4 Bad Feedback"
+
+					try:
+						self.plate_fback = ord(self.ser.read(1).decode('string_escape')) | (ord(self.ser.read(1).decode('string_escape')) << 8)
+						print self.plate_fback
+					except ValueError:
+						print "q2 Bad Feedback"
+
+					try:
+						self.temp = ord(self.ser.read(1).decode('string_escape')) | (ord(self.ser.read(1).decode('string_escape')) << 8)
+						print self.temp
+					except ValueError:
+						print "q2 Bad Feedback"
+
+					try:
+						self.humidity = ord(self.ser.read(1).decode('string_escape')) | (ord(self.ser.read(1).decode('string_escape')) << 8)
+						print self.humidity
+					except ValueError:
+						print "q2 Bad Feedback"
+
+					# self.q3_fback = ord(self.ser.read(1).decode('string_escape')) | (ord(self.ser.read(1).decode('string_escape')) << 8)
+					# self.q4_fback = ord(self.ser.read(1).decode('string_escape')) | (ord(self.ser.read(1).decode('string_escape')) << 8)
+					# self.plate_fback = ord(self.ser.read(1).decode('string_escape')) | (ord(self.ser.read(1).decode('string_escape')) << 8)					
+					# self.temp = ord(self.ser.read(1).decode('string_escape')) | (ord(self.ser.read(1).decode('string_escape')) << 8)
+					# self.humidity = ord(self.ser.read(1).decode('string_escape')) | (ord(self.ser.read(1).decode('string_escape')) << 8)
+					#		    self.pub_arm.publish(self.arm_feedback)
+					#		    self.pub_sci.publish(self.science_data)
 
 
 if __name__ == '__main__':
@@ -255,8 +317,8 @@ if __name__ == '__main__':
 	psoc = PSOC_class()
 
 	while not rospy.is_shutdown():
-		# psoc.set_rover_cmd()
-		# psoc.read_feedback()
+		psoc.set_rover_cmd()
+		psoc.read_feedback()
 		rate.sleep()
 
 	psoc.ser.close()
