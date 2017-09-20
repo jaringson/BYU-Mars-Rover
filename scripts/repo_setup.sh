f54005500356382605829f49780238cf41ff7f65
Seth Nielsen <sethmnielsen@gmail.com>
2017-09-20 15:28:52 -0600

    Added repo_setup.sh script for quick setup

--- scripts/repo_setup.sh
+++ scripts/repo_setup.sh
@@ -1,11 +1,13 @@
 sudo apt-get install ros-kinetic-gpsd-client
 sudo apt-get install ros-kinetic-razor-imu-9dof
 sudo apt-get install ros-kinetic-trac-ik-lib
+sudo apt-get install ros-kinetic-urdfdom-py
+git submodule update --init --recursive
 cd ~/BYU-Mars-Rover/shared_ws/
 catkin_make
 source devel/setup.bash
 cd ../onboard_ws/
-catkin_make
+catkin_make -DCATKIN_BLACKLIST_PACKAGES="zed_ros_wrapper;zed_wrapper"
 source devel/setup.bash
 cd ../rover_ws/
 catkin_make
